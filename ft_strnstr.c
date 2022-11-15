6a9d4907407cf186f56a69e6c035dc1b823fe2a9
AJOmega <76403255+AJOmega@users.noreply.github.com>
2022-11-15 20:52:38 +0000

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,24 +14,28 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	h;
-	size_t	n;
+	size_t	i;
+	size_t	j;
 
-	h = 0;
-	if (needle[0] == '\0' || len == 0)
+	if (!haystack || !needle)
+		return (NULL);
+	if (!needle || !needle[0])
 		return ((char *)haystack);
-	while (haystack[h] != '\0')
+	i = 0;
+	while (haystack[i] && i < len)
 	{
-		n = 0;
-		while (haystack[h + n] == needle[n] && (h + n) < len)
-		{
-			if (haystack[h + n] == '\0' && needle[n] == '\0')
-				return ((char *)&haystack[h]);
-			n++;
-		}
-		if (needle[n] == '\0')
-			return ((char *)haystack + h);
-		h++;
+		j = 0;
+		while (haystack[i + j] && needle[j] &&
+				i + j < len && haystack[i + j] == needle[j])
+			j++;
+		if (!needle[j])
+			return ((char *)(haystack + i));
+		i++;
 	}
-	return (0);
+	return (NULL);
 }
+
+/*int	main(void)
+{
+	printf("%s\n", ft_strnstr("a", "ab", 1));
+}*/
